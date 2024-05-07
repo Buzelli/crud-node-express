8d8da787de8b6c1d78a2cf469378ac0fa9d0e52f
Andre buzelli <buzelli4@gmail.com>
2024-05-06 21:41:49 -0300

    adicionando get, post,put, delete

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,11 @@
 const express  = require('express');
 const router = express.Router();
 
+const CarroController = require('./controllers/CarroController')
+
+router.get('/carros', CarroController.buscarTodos);
+router.get('/carro/:codigo', CarroController.buscarUm);
+router.post('/carro', CarroController.adicionar);
+router.put('/carro/:codigo', CarroController.alterar);
+router.delete('/carro/:codigo', CarroController.remover)
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const server  = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({extended :false}));
 
+server.use('/api',routes);
+
 server.listen(process.env.Port, ()=>{
     console.log(`Servidor rodando na porta ${process.env.Port}`);
 })
